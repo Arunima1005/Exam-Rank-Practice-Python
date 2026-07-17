39ce763a684bd08e6f926514bce69d07b51009f5
Arunima Chowdhury <archowdh@f0r3s26.codam.nl>
2026-07-17 19:35:55 +0200

    started is rotate

---
+++ Exam Rank 03/is_rotation.py
@@ -0,0 +1,11 @@
+def is_rotation(arr1: list, arr2: list) -> bool:
+    pass
+
+
+print(is_rotation([1, 2, 3, 4, 5], [4, 5, 1, 2, 3]))
+print(is_rotation([1, 2, 3, 4, 5], [5, 1, 2, 3, 4]))
+print(is_rotation([1, 2, 3], [3, 2, 1]))
+print(is_rotation([1, 2], [1, 2, 3]))
+print(is_rotation([], []))
+
+## true true false false true
