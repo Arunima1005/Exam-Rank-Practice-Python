894b3c9bc1ff0ea70b90c75f42f6a49eb5fe0de4
Arunima Chowdhury <chowdhury.arunima@gmail.com>
2026-07-21 23:53:59 +0200

    modified 136

--- Leetcode/136_single_number.py
+++ Leetcode/136_single_number.py
@@ -10,3 +10,16 @@ class Solution:
 
 s = Solution()
 print(s.singleNumber([1,0,1]))
+
+
+# using dict
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+#         counts = {}
+
+#         for num in nums:
+#             counts[num] = counts.get(num, 0) + 1
+
+#         for key, value in counts.items():
+#             if value == 1:
+#                 return key
